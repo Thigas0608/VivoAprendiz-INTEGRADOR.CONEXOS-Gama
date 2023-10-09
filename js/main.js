a1c36d52434810ca8b08061e4e07ae789d2a9472
Geovanealvesr <122214333+Geovanealvesr@users.noreply.github.com>
2023-10-09 05:42:43 -0300

    Criando modails de sucess

--- js/main.js
+++ js/main.js
@@ -32,52 +32,36 @@ function fazerSolicitacaoGET() {
             });
     }
 }
-function fazerSolicitacaoGET() {
-    var pedidoId = document.getElementById('codigo').value;
-    
-    // Verifica se o campo do código do pedido está vazio
-    if (pedidoId.trim() === '') {
-        // Se estiver vazio, mostre o modal de aviso
-        var codigoVazioModal = new bootstrap.Modal(document.getElementById('codigoVazioModal'));
-        codigoVazioModal.show();
-    } else {
-        var url = 'http://localhost:3000/ordens/' + pedidoId;
+// Função para mostrar o modal de sucesso
+function mostrarModalSucesso(data) {
+    // Preencha os campos do modal com os dados recebidos
+    document.getElementById('modal-nome').textContent = data.nome;
+    document.getElementById('modal-cpf').textContent = data.cpf;
+    document.getElementById('modal-telefone').textContent = data.telefone;
+    document.getElementById('modal-email').textContent = data.email;
+    document.getElementById('modal-plano').textContent = data.plano;
+    document.getElementById('modal-horario').textContent = data.horario;
+    document.getElementById('modal-status').textContent = data.status;
 
-        axios.get(url)
-            .then(function (response) {
-                var data = response.data; 
-
-                document.getElementById('modal-nome').textContent = data.nome;
-                document.getElementById('modal-cpf').textContent = data.cpf;
-                document.getElementById('modal-telefone').textContent = data.telefone;
-                document.getElementById('modal-email').textContent = data.email;
-                document.getElementById('modal-plano').textContent = data.plano;
-                document.getElementById('modal-horario').textContent = data.horario;
-                document.getElementById('modal-status').textContent = data.status;
-
-                // Abra o modal
-                var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
-                myModal.show();
-            })
-            .catch(function (error) {
-                console.error('Erro ao fazer a solicitação GET:', error);
-            });
-    }
+    // Abra o modal de sucesso
+    var modalSucesso = new bootstrap.Modal(document.getElementById('exampleModal'));
+    modalSucesso.show();
 }
 
-function enviarDados() {
+// Função para fazer a solicitação POST
+function fazerSolicitacaoPOST() {
     // Recupere os valores dos campos do formulário
     var nome = document.getElementById('nome').value;
     var cpf = document.getElementById('cpf').value;
     var telefone = document.getElementById('telefone').value;
     var email = document.getElementById('email').value;
     var plano = document.getElementById('plano').value;
-    var horarioSelect = document.getElementById('horario'); // Altere o ID para 'horario'
+    var horarioSelect = document.getElementById('horario');
 
     // Obtenha o valor selecionado do campo de seleção de horário
     var horario = horarioSelect.options[horarioSelect.selectedIndex].value;
 
-    // Resto do código permanece inalterado
+    // Construa o objeto de dados
     const dados = {
         nome: nome,
         cpf: cpf,
@@ -88,11 +72,16 @@ function enviarDados() {
         status: "Em Andamento"
     };
 
+    // URL da solicitação POST
     const url = "http://localhost:3000/ordens";
 
+    // Envie a solicitação POST usando Axios
     axios.post(url, dados)
         .then(function (response) {
             console.log("Solicitação POST bem-sucedida:", response.data);
+            
+            // Após o sucesso da solicitação POST, chame a função para mostrar o modal de sucesso
+            mostrarModalSucesso(response.data);
         })
         .catch(function (error) {
             console.error("Erro na solicitação POST:", error);
